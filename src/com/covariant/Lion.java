c06639cc204d0dfed47f84e9b290c68cd89fd744
Manish Gusain <gusainmanish68@gmail.com>
2022-10-17 16:54:50 +0530

    first commit

---
+++ src/com/covariant/Lion.java
@@ -0,0 +1,9 @@
+package com.covariant;
+
+public class Lion extends Animal{
+
+	public Lion roar() {
+		System.out.println("Lion is roaring");
+		return new Lion();
+	}
+}
---
+++ src/com/covariant/MainClass.java
@@ -0,0 +1,15 @@
+package com.covariant;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+//		Animal animal=new Lion();
+//		Animal roar = animal.roar();
+//		System.out.println(roar.getClass());
+		
+		Animal a=new Lion();
+		a.roar();
+		
+	}
+
+}
